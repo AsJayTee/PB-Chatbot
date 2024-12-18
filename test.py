cc115414ce72c1b701ece51cb1190b7afddbf979
AsJayTee <jinthauofficial@gmail.com>
2024-12-18 09:40:25 +0800

    Updated testing file

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
-import logging
 from main import main
 
-logging.basicConfig(
-    level = logging.INFO,
-    format = '%(asctime)s - %(levelname)s - %(message)s')
 app = main(debug = True)
 while (query := input("Input: ")):
-    print(app.chat(query))
+    print(f"Response: {app.chat(query)}")
 cost = app.get_new_costs()
 print(f"Embedding Cost: {(embed_cost := cost.get('embed_cost'))}")
 print(f"GPT-4o Cost: {(gpt_4o_cost := cost.get('gpt_4o_cost'))}")
